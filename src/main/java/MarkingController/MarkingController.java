57e579e965c852bb28fd7d4b9187edeb23dc62e5
Stephanie Lepuschitz <ic21b013@technikum-wien.at>
2022-12-21 19:23:38 +0100

    commit rest of code

--- src/main/java/MarkingController/MarkingController.java
+++ src/main/java/MarkingController/MarkingController.java
@@ -1,7 +1,33 @@
 package MarkingController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 public class MarkingController {
+    /* Percentage Value (input)
+    >= 88% = 1
+    >= 75% and < 88% = 2
+    >= 63% and < 75% = 3
+    >= 50% and < 63% = 4
+    < 50% = 5
+     */
+
+    double percentage;
+    int grade;
+
+    if(percentage < 50){
+        grade = 5;
+    }else if(percentage >= 50 &&){
+
+    }
+
 
 }
+
+
+
+
+
+
+
